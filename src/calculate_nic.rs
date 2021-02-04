2d5e55eb918aedf95890e4ac40629639bddb9ccd
geri1701 <67984144+geri1701@users.noreply.github.com>
2021-02-04 16:07:52 +0100

    Update calculate_nic.rs

--- src/calculate_nic.rs
+++ src/calculate_nic.rs
@@ -1,4 +1,4 @@
-/// Calculates nicotine ammount
+/// Calculates nicotine amount
 pub fn calculate_nic(target_v: f64, want_nic_conc: f64, base_conc: f64) -> f64 {
     (target_v * want_nic_conc) / base_conc
 }
