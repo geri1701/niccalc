077d9e6a8efd5820f508e6b3384c7a4b483e341b
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-10-17 15:45:33 +0300

    add light and dark themes

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ use {
     comfy_table::{modifiers, presets, Table},
     fltk::{
         app,
-        enums::{CallbackTrigger, Color, Cursor, Event, Font, FrameType},
+        enums::{Align, CallbackTrigger, Color, Cursor, Event, Font, FrameType},
         frame::Frame,
         group::{Flex, Wizard},
         image::PngImage,
         input::{Input, InputType},
-        menu::{MenuButton, MenuButtonType},
+        menu::{Choice, MenuButton, MenuButtonType},
         misc::Chart,
         prelude::*,
         text::{TextBuffer, TextDisplay, WrapMode},
+        valuator::{Counter, CounterType},
         window::Window,
     },
     std::{cell::RefCell, rc::Rc},
@@ -21,6 +22,7 @@ use {
 
 const PAD: i32 = 10;
 const HEIGHT: i32 = PAD * 3;
+const WIDTH: i32 = HEIGHT * 3;
 
 enum Update {
     NicCal = 41,
@@ -35,7 +37,7 @@ impl Update {
 fn main() -> Result<(), FltkError> {
     let state = Rc::new(RefCell::new(models::Model::default()));
     const UPDATE: Event = Update::NicCal.event();
-    let app = app::App::default().with_scheme(app::Scheme::Plastic);
+    let app = app::App::default();
     cascade!(
         Window::default().with_size(640, 360).center_screen();
         ..size_range(640, 360, 0, 0);
@@ -178,6 +180,7 @@ fn main() -> Result<(), FltkError> {
                 ));
                 ..end();
             ));
+            ..add(&page_settings());
             ..add(&cascade!(
                 Flex::default_fill().with_label("Info");
                 ..set_margin(PAD);
@@ -196,16 +199,6 @@ fn main() -> Result<(), FltkError> {
         ..end();
     )
     .show();
-    for (color, hex) in [
-        (Color::Yellow, 0xb58900),
-        (Color::Red, 0xdc322f),
-        (Color::Blue, 0x268bd2),
-        (Color::Green, 0x859900),
-    ] {
-        let (r, g, b) = Color::from_hex(hex).to_rgb();
-        app::set_color(color, r, g, b);
-    }
-    app::set_font(Font::CourierBold);
     app.run()
 }
 
@@ -250,4 +243,104 @@ fn add_menu(wizard: &mut Wizard, event: Event) -> bool {
         }
         _ => false,
     }
+}
+
+fn page_settings() -> Flex {
+    cascade!(
+        Flex::default_fill().with_label("Settings");
+        ..set_margin(PAD);
+        ..set_pad(PAD);
+        ..add(&Frame::default());
+        ..fixed(&cascade!(
+            Flex::default_fill().column();
+            ..set_pad(PAD);
+            ..set_margin(PAD);
+            ..add(&Frame::default());
+            ..add(&cascade!(
+                Flex::default_fill();
+                ..fixed(&Frame::default(), WIDTH);
+                ..add(&cascade!(
+                    Flex::default_fill().column();
+                    ..set_color(Color::Foreground);
+                    ..set_pad(PAD);
+                    ..fixed(&cascade!(
+                        Choice::default().with_label("Theme");
+                        ..add_choice("Light|Dark");
+                        ..set_value(0);
+                        ..set_callback(move |choice| {
+                            let color = [
+                                [ //LIGHT
+                                    0xF6F5F4, //set_background_color
+                                    0xFFFFFF, //set_background2_color
+                                    0x404040, //set_foreground_color
+                                    0xb58900, //set_inactive_color
+                                ],
+                                [ //DARK
+                                    0x404040, //set_background_color
+                                    0x3B3B3B, //set_background2_color
+                                    0xF6F5F4, //set_foreground_color
+                                    0x268bd2, //set_inactive_color
+                                ],
+                            ][choice.value() as usize];
+                            app::set_scheme(match choice.value() != 0 {
+                                true => app::Scheme::Base,
+                                false => app::Scheme::Oxy,
+                            });
+                            let (r, g, b) = Color::from_hex(color[0]).to_rgb();
+                            app::set_background_color(r, g, b);
+                            let (r, g, b) = Color::from_hex(color[1]).to_rgb();
+                            app::set_background2_color(r, g, b);
+                            let (r, g, b) = Color::from_hex(color[2]).to_rgb();
+                            app::set_foreground_color(r, g, b);
+                            let (r, g, b) = Color::from_hex(color[2]).to_rgb();
+                            app::set_selection_color(r, g, b);
+                            let (r, g, b) = Color::from_hex(color[3]).to_rgb();
+                            app::set_inactive_color(r, g, b);
+                            for (color, hex) in [
+                                (Color::Yellow, 0xb58900),
+                                (Color::Red, 0xdc322f),
+                                (Color::Magenta, 0xd33682),
+                                (Color::Blue, 0x268bd2),
+                                (Color::Cyan, 0x2aa198),
+                                (Color::Green, 0x859900),
+                            ] {
+                                let (r, g, b) = Color::from_hex(hex).to_rgb();
+                                app::set_color(color, r, g, b);
+                            }
+                            app::set_visible_focus(false);
+                            app::redraw();
+                        });
+                        ..do_callback();
+                    ), HEIGHT);
+                    ..fixed(&cascade!(
+                        Choice::default().with_label("Font name");
+                        ..add_choice(&app::fonts().join("|"));
+                        ..set_value(5);
+                        ..set_callback(move |choice| {
+                            app::set_font(Font::by_index(choice.value() as usize));
+                        });
+                        ..do_callback();
+                    ), HEIGHT);
+                    ..fixed(&cascade!(
+                        Counter::default().with_label("Font size");
+                        ..set_type(CounterType::Simple);
+                        ..set_align(Align::Left);
+                        ..set_range(1_f64, 14f64);
+                        ..set_precision(0);
+                        ..set_value(14f64);
+                        ..set_callback(move |counter| {
+                            app::set_font_size(counter.value() as i32);
+                        });
+                        ..do_callback();
+                    ), HEIGHT);
+                    ..end();
+                ));
+                ..end();
+            ));
+            ..add(&Frame::default());
+            ..end();
+        ), WIDTH * 3);
+        ..add(&Frame::default());
+        ..end();
+    )
 }
