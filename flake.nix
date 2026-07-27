71c1a3f39cc677a6b6c85816823ea627034e1667
geri1701 <gschwa@gmx.net>
2026-07-27 10:56:34 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,79 @@
 {
-  inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
+  description = "A nicotine calculator for e-cigarette liquids";
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              pre-commit
-              rustPackages.clippy
-              xorg.libX11
-              xorg.libXext
-              xorg.libXinerama
-              xorg.libXcursor
-              xorg.libXrender
-              xorg.libXfixes
-              xorg.libXft
-              pango
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "niccalc";
+            version = "0.9.17";
+            src = ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkg-config
             ];
-            nativeBuildInputs = with pkgs; [ pkg-config openssl.dev curl ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+
+            buildInputs = [
+              pkgs.fontconfig
+              pkgs.libx11
+              pkgs.libxcursor
+              pkgs.libxext
+              pkgs.libxfixes
+              pkgs.libxft
+              pkgs.libxinerama
+              pkgs.libxrender
+              pkgs.pango
+            ];
+
+            meta = {
+              description = "A tool to calculate nicotine amount needed for an e-cigarette liquid";
+              homepage = "https://github.com/geri1701/niccalc";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "niccalc";
+            };
           };
+        }
+      );
+
+      checks = forAllSystems (system: {
+        default = self.packages.${system}.default;
       });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = [
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.rustc
+              pkgs.rustfmt
+            ];
+          };
+        }
+      );
+    };
 }
